6e0e4f9070df55259fdbee1ebbd2621c5554af83
KwonNamSon <kwon37xi@gmail.com>
2020-07-17 15:25:04 +0900

    first commit

---
+++ research-spring-data-jdbc/src/test/java/kr/pe/kwonnam/researchspringboot23/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.kwonnam.researchspringboot23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
