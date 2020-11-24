0022b59aeb09368f046136d8349e0c9c0d1c351f
KwonNam Son <kwon37xi@gmail.com>
2020-11-24 18:46:46 +0900

    Squiggly.init 관련 설명 추가

--- research-spring-webmvc-squiggly/src/main/java/kr/pe/kwonnam/researchspringboot23/squiggly/SquigglyDemoApplication.java
+++ research-spring-webmvc-squiggly/src/main/java/kr/pe/kwonnam/researchspringboot23/squiggly/SquigglyDemoApplication.java
@@ -26,6 +26,8 @@ public class SquigglyDemoApplication {
 
     public static void main(String[] args) {
         final ConfigurableApplicationContext context = SpringApplication.run(SquigglyDemoApplication.class, args);
+        // WebMvcConfig#extendMessageConverters 에서 HttpMessageConverter중에서 MappingJackson2HttpMessageConverter 인
+        // 것을 찾아서 해도 된다.
 
         for (MappingJackson2HttpMessageConverter converter : context.getBeansOfType(MappingJackson2HttpMessageConverter.class).values()) {
             final ObjectMapper objectMapper = converter.getObjectMapper();
